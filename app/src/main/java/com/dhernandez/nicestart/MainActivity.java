1aa60a4565b74b806b5d5d42541ad56e704b7b3a
am4DAM2-Diego <dhercan391@gmail.com>
2024-11-18 12:27:34 +0100

    Conectado con botones la pantalla de Profile.

--- app/src/main/java/com/dhernandez/nicestart/MainActivity.java
+++ app/src/main/java/com/dhernandez/nicestart/MainActivity.java
@@ -1,6 +1,13 @@
 package com.dhernandez.nicestart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +17,63 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView myContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        myContext = findViewById(R.id.textView2);
+        registerForContextMenu(myContext);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        if (item.getItemId()==R.id.item1){
+            Toast.makeText(this, "Op1: Copiando", Toast.LENGTH_SHORT).show();
+            return true;
+        }else if (item.getItemId()==R.id.item2){
+            Toast.makeText(this, "Op1: Descargando", Toast.LENGTH_SHORT).show();
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+   public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+        return true;
+    }
+
+
+
+    @Override
+   public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id==R.id.perfil){
+            Intent intent = new Intent(MainActivity.this, Profile.class);
+            startActivity(intent);
+        }else if(id==R.id.logout) {
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            startActivity(intent);
+        }else if(id==R.id.mas) {
+            Toast.makeText(this, "Mas cositas coming", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/com/dhernandez/nicestart/Profile.java
+++ app/src/main/java/com/dhernandez/nicestart/Profile.java
@@ -12,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class Profile extends AppCompatActivity {
 
     @Override
@@ -24,7 +26,7 @@ public class Profile extends AppCompatActivity {
         Glide.with(this)
                 // .load(R.drawable.girl)
                 .load("https://images.unsplash.com/photo-1729009528556-4c0a4b834202?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-                .transition(DrawableTransitionOptions.withCrossFade(1500))
+                .transition(DrawableTransitionOptions.withCrossFade(1100))
                 //.centerCrop()
                 .circleCrop()
 
